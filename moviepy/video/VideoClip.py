7b991ea1640c52b385ac71960dd1a7876eda1307
Pranjalya Tiwari <pranjalyawarrior@gmail.com>
2024-08-21 10:29:28 +0530

    add torch support

--- moviepy/video/VideoClip.py
+++ moviepy/video/VideoClip.py
@@ -699,7 +699,8 @@ class VideoClip(Clip):
             pos[1] = D[pos[1]]
 
         pos = map(int, pos)
-        return blit(im_img, picture, pos, mask=im_mask)
+        # return blit(im_img, picture, pos, mask=im_mask)
+        return blit_gpu(img, picture, pos, mask=im_mask, ismask=False)
 
     def add_mask(self):
         """Add a mask VideoClip to the VideoClip.
